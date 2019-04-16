5d7b818f08cd0b11201d5dbcf8e320c8e15c9c18
Josh Diaz <josh@iserveproducts.com>
2019-04-15 21:35:28 -0600

    making call for expenses

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { IonCard, IonCardTitle } from '@ionic/react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import moment from 'moment'
+
+const urlBase = 'expense-tracker/api/v1/expenses';
 
 const Home = () => {
-
-  let data = [
-    2017,
-    2018,
-    2019
-  ]
 
   const [years, setYears] = useState([]);
 
@@ -17,12 +15,25 @@ const Home = () => {
   }, [])
 
   const getYears = () => {
-    setYears(data)
+    axios.get(urlBase).then(response => {
+      let arr = response.data.data;
+      getUniqueYears(arr);
+    });
+  }
+
+  const getUniqueYears = (arr) => {
+    let unique = arr.map(a => {
+      return moment(a.year).format('YYYY')
+    })
+    unique = [...new Set(unique.map(u => {
+      return u
+    }))]
+    setYears(unique);
   }
 
   return (
     <div className="home-page">
-      { years.map((e, i) => (
+      {years.map((e, i) => (
         <Link to={`/expenses/${e}`} key={i}>
           <IonCard padding margin>
             <IonCardTitle>{e}</IonCardTitle>
@@ -32,5 +43,5 @@ const Home = () => {
     </div>
   );
 }
- 
+
 export default Home
